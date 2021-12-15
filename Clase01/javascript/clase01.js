f2cbc3a819f6943e7b79a93ff836964ede74af5f
Sh4rkD <20006132@es.uveg.edu.mx>
2021-12-14 17:20:57 -0700

    Realizando el primer reto

---
+++ Clase01/javascript/clase01.js
@@ -0,0 +1,4 @@
+let userName = prompt("What's your name?");
+let userAge = prompt("How old are you?");
+let userBirthYear = 2021;
+alert("Hey "+userName+" you are "+userAge+" years old and your age of birth is "+(userBirthYear-(parseInt(userAge)))+" at the end of this year.");
